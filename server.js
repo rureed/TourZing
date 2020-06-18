6e75ceeba2a8f591ad1ec9a4f94d5512ead60961
WillV1 <wmvanhook@gmail.com>
2020-06-18 18:54:18 -0400

    Upload package 1-2 of updates

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const PORT = process.env.PORT || 7000;
+const db = require("./models");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.json());
+
+
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+db.sequelize.sync().then(function() {
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+});
