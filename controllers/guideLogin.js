aed03142893f42b14d7c5771fece98746aa1b55b
WillV1 <wmvanhook@gmail.com>
2020-06-28 21:18:03 -0400

    Update guide login

--- controllers/guideLogin.js
+++ controllers/guideLogin.js
@@ -3,23 +3,27 @@ const db = require('../models');
 
 
 module.exports = (req, res) => {
-    const { email, password } = req.body;
+    // const { email, password } = req.body;
 
-    db.Guide.findOne({ username: username }, (error, user) => {
-        if (user) {
-            bcrypt.compare(password, user.password, (error, same) => {
-                if (same) {
-                    req.session.userId = user._id
-                    res.redirect('/accountGuide')
-                }
-                else {
-                    res.redirect('/guide/login')
-                }
-            })
-        }
-        else {
-            res.redirect('guide/login')
-        }
-    })
+    // db.Guide.findOne({ email: email }, (error, user) => {
+    //     if (user) {
+    //         bcrypt.compare(password, user.password, (error, same) => {
+    //             if (same) {
+    //                 req.session.userId = user._id
+    //                 res.redirect('/accountGuide')
+    //             }
+    //             else {
+    //                 res.redirect('/guide/login')
+    //             }
+    //         })
+    //     }
+    //     else {
+    //         res.redirect('guide/login')
+    //     }
+    // })
+
+    
+        res.render('loginGuide')
+    
 
 };
--- controllers/storeGuide.js
+++ controllers/storeGuide.js
@@ -9,13 +9,18 @@ module.exports = (req, res) => {
         password: req.body.password,
         phone: req.body.phone,
         city: req.body.city},
-    (error,user) =>{
-        if(error) {
-            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
-            req.flash('validationErrors', validationErrors)
-            req.flash('data', req.body)
-            return res.redirect('./accountGuide')
-        }
+        ).then((user) => {
+            // if(error){
+            //     const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
+            //     req.flash('validationErrors', validationErrors)
+            //     req.flash('data',{
+            //         firstName: req.body.firstName,
+            //         lastName: req.body.lastName,
+            //         email: req.body.email,
+            //         password: req.body.password
+            //     })
+            //     return res.redirect('/auth/register')
+            // }else {
         res.redirect('/accountGuide')
     })
 }
